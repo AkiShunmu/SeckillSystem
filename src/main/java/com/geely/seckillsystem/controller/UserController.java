f5c1f58a7230c8104595f42f8e0d44179c272a10
Qishun.Zhou <qishun.zhou@geely.com>
2021-03-05 13:14:54 +0800

    后端传输数据，需完成前端接收

--- src/main/java/com/geely/seckillsystem/controller/UserController.java
+++ src/main/java/com/geely/seckillsystem/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.geely.seckillsystem.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.geely.seckillsystem.pojo.User;
 import com.geely.seckillsystem.service.IUserService;
-import jdk.nashorn.internal.parser.JSONParser;
+import com.sun.net.httpserver.HttpExchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +26,7 @@ public class UserController {
 
     @Autowired
     private IUserService userService;
+    private HttpExchange request;
 
     @RequestMapping("/webtest")
     public String webtest(Model model) {
@@ -34,9 +36,11 @@ public class UserController {
         return "webtest";
     }
 
+    //根据 id,nickname 查询用户信息
     @RequestMapping("/getUser")
     public String getUser(String id, String nickname) {
         List<User> list =  userService.getUser(id, nickname);
+        JSONArray.toJSONString(list);
         return "User";
     }
 
